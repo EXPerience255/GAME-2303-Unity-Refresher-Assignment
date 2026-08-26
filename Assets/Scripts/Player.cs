c05f11397ba79db6d4b75ad544ac52b3521120d9
EXPerience255 <71566449+EXPerience255@users.noreply.github.com>
2026-08-26 12:19:16 -0500

    applied model and animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,25 +4,41 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     private Rigidbody rb;
+    private Animator anim;
     private Vector2 moveVector;
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask floorLayerMask;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        anim.SetFloat("moveSpeed", moveVector.magnitude);
+        transform.forward = new Vector3(moveVector.x, 0, moveVector.y);
     }
 
     private void FixedUpdate()
     {
         rb.AddForce(new Vector3(moveVector.x, 0, moveVector.y) * moveSpeed, ForceMode.Acceleration);
+        if (Physics.Raycast(transform.position, Vector3.down, 1f, floorLayerMask))
+            anim.SetBool("isGrounded", true);
+        else
+            anim.SetBool("isGrounded", false);
     }
 
     public void Jump(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
+        {
             rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            anim.SetTrigger("jump");
+        }
     }
 
     public void Move(InputAction.CallbackContext ctx)
